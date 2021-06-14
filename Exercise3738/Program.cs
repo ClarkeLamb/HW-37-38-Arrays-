56e60809edcf902036dbde8a350ed1197870cfcb
clarke <cf.lamb25@gmail.com>
2021-06-14 17:22:57 -0400

    First commit HW 37 & 38

---
+++ Exercise3738/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Exercise3738
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //37
+            int sum = 0;
+            int sums = 0;
+            int average = 0;
+            string answer = "";
+
+            int[] fiveNumbers = new int[5];
+            do
+            {
+                for (int i = 0; i < fiveNumbers.Length; i++)
+                {
+                    Console.Write("Please enter a number: ");
+                    fiveNumbers[i] = int.Parse(Console.ReadLine());
+
+                }
+                for (int i = 0; i < fiveNumbers.Length; i++)
+                {
+                    sum += fiveNumbers[i];
+
+                }
+                Console.WriteLine("Your sum is:  " + sum);
+
+                Console.WriteLine("Would you like to continue? y/n");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower() != "n");
+            Console.WriteLine("Goodbye");
+
+
+
+
+            //38
+            int[] numbers = new int[5];
+            do
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.Write("Please enter a number: ");
+                    numbers[i] = int.Parse(Console.ReadLine());
+
+                }
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    sums += numbers[i];
+                    average = sums / 5;
+
+                }
+                Console.WriteLine("Your average is:  " + average);
+
+                Console.WriteLine("Would you like to continue? y/n");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower() != "n");
+            Console.WriteLine("Goodbye");
+
+        }
+    }
+}
+
+
+
+
